add83966740fce7412d6cf7f9e02ea04ad634daf
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-19 18:50:41 +0000

    blogs have been added

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
 const Home = () => {
+    const [blogs,setBlogs]=useState([
+        {title:'MY new Website',body:'Lorem upsum...',author:'mario',id:1},
+        {title:'Welcome Party',body:'Lorem upsum...',author:'Yoshi',id:2},
+        {title:'Web dev top tips',body:'Lorem upsum...',author:'mario',id:3}
+    ])
+    const handleDelete=(id)=>{
+        const newBlog=blogs.filter(blog=>blog.id!==id)
+        setBlogs(newBlog)
+    }
+
     return ( 
+    
 <div className="home">
-    <h2>Home Page</h2>
+        <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
 </div>
-
-
      );
 }
  
 export default Home;
+
+
+// const handleClickAgain=(e,name)=>{
+//     console.log(e.type+`hello ${name}`)
+// }
+
+// {/* <button onClick={(e)=>handleClickAgain(e,'mario')}>Click me again</button> */}
